28b3a330f527e9d8c47cef21367b036ed86cf316
BiggyG <alexcraft2003@gmail.com>
2019-09-30 15:50:34 -0500

    i don't really even know we are typing now

--- learning.py
+++ learning.py
@@ -1,2 +1,10 @@
+# What is an octothrope
+# <- This is an octothrope.
+# What is the octothrope used for in python?
+# Notes, also known in the programmingcommunity as Comments
+
 print("hello world")
 print("howdy y'all")
+
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
